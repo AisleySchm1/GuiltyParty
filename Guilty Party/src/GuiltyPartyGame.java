85a0294d6c86d4c500a2b967cd096a49d0ece616
aisley.schmeisser <aisley.schmeisser@MU-C1-16.mullen.pvt.k12.co.us>
2024-09-25 12:03:29 -0600

    Initial Commit

---
+++ Guilty Party/src/GuiltyPartyGame.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class GuiltyPartyGame
+	{
+
+		public static void main(String[] args)
+			{
+				Scanner userStringInput = new Scanner (System.in);
+				Scanner userIntInput = new Scanner (System.in);
+				
+				System.out.println("Welcome to Guilty Party! This is a murder mystry game where you choose your character and try and solve a murder before its too late! Please hit eneter to recieve instructions and choose your character.");
+
+			}
+
+	}
